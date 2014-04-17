0f8abbbc52d0d2fe458af6ad51c7b7b647e04b3b
Theodore J Mason <masont@cat.pdx.edu>
2014-04-16 17:54:16 -0700

    took out some useless files

--- bot.bash
+++ bot.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # original code by krinkle (cmurphy) edited by dom as a fork
+# initialize variables from passed in variables
 mkfifo .botfile
 export foodfile="foods.txt"
 export chan=$1
@@ -7,6 +8,7 @@ if [ "$2" != '' ] && [ -f "$2" ] ; then
   export key=`cat $2`
 fi
 
+# function to read in from a text file and echo to irc the contents of said file
 readfoods(){
 
     cat foods.txt | while read line 
@@ -14,67 +16,49 @@ readfoods(){
         echo "PRIVMSG #$chan :$line" >> .botfile
         sleep 1
     done <foods.txt
-# case `date +%H` in 
-#   14) sed -n '1,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;& 
-#   15) sed -n '2,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   16) sed -n '3,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   17) sed -n '4,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   18) sed -n '5,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   19) sed -n '6,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   20) sed -n '7,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   21) sed -n '8,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   22) sed -n '9,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   23) sed -n '10,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   00) sed -n '11,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   01) sed -n '12,13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   *) sed -n '13' < foods.txt | while read line 
-#       do echo "PRIVMSG #$chan :$line" >> .botfile; sleep 1 ;&
-#   esac
-
-
 }
 
+
 tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; do
+
+  # Tell irc the things it needs to know to create a user named needabotbot
   if [[ -z $started ]] ; then
     echo "USER needabotbot needabotbot needabotbot :needabotbot" >> .botfile
     echo "NICK needabotbot" >> .botfile
     echo "JOIN #$chan $key" >> .botfile
     started="yes"
   fi
+
+  # Read in and echo irc
   read irc
   echo $irc
+
+  # Whenever irc server send "PING" to user (needabotbot), send back "PONG"
+  # so that irc doesn't think you're dced from the server
   if `echo $irc | cut -d ' ' -f 1 | grep PING > /dev/null`; then
     echo "PONG" >> .botfile 
+  
+  # Whenver nabb sees 'need a bot' somewhere in irc chat, reply with "you could write it"
   elif `echo $irc | grep PRIVMSG | grep 'need a bot\b' > /dev/null` ; then
     nick="${irc%%!*}"; nick="${nick#:}"
     if [[ $nick != 'needabotbot' ]] ; then
-#      chan=`echo $irc | cut -d ' ' -f 3`
       echo "PRIVMSG #$chan :you could write it..." >> .botfile
     fi
+
+  # Whenever nabb sees 'bot needs to' somewhere in irc chat, reply with "submit a pull request"
   elif `echo $irc | grep PRIVMSG | grep 'bot needs to\b' > /dev/null` ; then
 
    nick="${irc%%!*}"; nick="${nick#:}"
    echo "PRIVMSG #$chan :submit a pull request!" >> .botfile
-
+  
+  # Whenever nabb sees 'bot should' somewhere in irc chat, reply with "make an issue"
   elif `echo $irc | grep PRIVMSG | grep 'bot should\b' > /dev/null` ; then
   
     nick="${irc%%!*}"; nick="${nick#:}"
     echo "PRIVMSG #$chan :make an issue!" >> .botfile
 
-
-#  elif `echo $irc | grep PRIVMSG | grep 'nabb: what is open?' > /dev/null` ; then
+  # Whenever nabb is pinged by someone and sees the word open in the same line, replies with all open food
+  # places near here
   elif `echo $irc | grep PRIVMSG | grep 'nabb:'| grep 'open' > /dev/null` ; then
         nick="${irc%%!*}"; nick="${nick#:}"
         readfoods
@@ -82,13 +66,6 @@ tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; 
   elif `echo $irc | grep PRIVMSG | grep 'needabotbot:'| grep 'open' > /dev/null` ; then
         nick="${irc%%!*}"; nick="${nick#:}"
         readfoods
-
-      
-#      cat foods.txt | sed "s/^/PRIVMSG #$chan :/" >> .botfile     
-#  elif `echo $irc | grep PRIVMSG | grep 'nabb: add ' > /dev/null` ; then  
-#      sed 's/
-  
-  
   fi       
  
 done
