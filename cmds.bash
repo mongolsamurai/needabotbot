e1cea3ee7c76cd739a180f2794e8b45a3535bf19
Theodore Mason <masont@cecs.pdx.edu>
2015-02-17 14:43:29 -0800

    add spam flagging

--- cmds.bash
+++ cmds.bash
@@ -13,15 +13,24 @@ elif `echo $saying | grep -i 'nabb: source' > /dev/null` ; then
     echo "PRIVMSG $chan :$nick: http://goo.gl/IIQt53 is my source file"
     echo "$nick: $saying" >> ../botideas
 elif `echo $saying | grep -i '!dropthemic' > /dev/null` ; then
-    echo "PRIVMSG $chan : OOOOH! You just got *served*!"
+    echo "PRIVMSG $chan :OOOOH! You just got *served*!"
 elif `echo $saying | grep -i 'nabb: are you a bot?' > /dev/null` ; then
     echo "PRIVMSG $chan :$nick: I sure am :)"
 elif `echo $saying | grep -i 'nabb: make me a sandwich' > /dev/null` ; then
     if [ "$nick" = dom ] ; then
         echo "PRIVMSG $chan :$nick: Sure thing, buddy."
+    elif [ "$nick" = relsqui ] ; then
+        echo "PRIVMSG $chan :$nick: Alright, I *guess* I can make you a sandwich."
     else
        echo "PRIVMSG $chan :$nick: I'll never be used by the likes of you!"
     fi        
+elif `echo $saying | grep -i "nabb: spam\b" > /dev/null` ; then
+    ticket=`echo $saying | cut -d ' ' -f 3` 
+    if [ "$nick" = dom ] ; then
+        if [[ $ticket =~ ^[0-9]{6}$ ]] ; then 
+            snot -F SPAM $ticket
+        fi 
+    fi
 elif `echo $saying | grep -i '!kekball' > /dev/null` ; then
     kekball=`sed -n "$randint"p kekball`
     echo "PRIVMSG $chan :$nick: $kekball"
