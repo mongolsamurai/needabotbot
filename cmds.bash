9ecc55a6fc9d3e735c4d5b40e79cd7aee7963324
Theodore Mason <masont@cecs.pdx.edu>
2015-11-09 11:51:24 -0800

    clean up code, add more easter eggs

--- cmds.bash
+++ cmds.bash
@@ -7,7 +7,7 @@ if `echo $saying | grep -i '\bneed a\b' | grep -i '\bbot\b' > /dev/null` ; then
     echo "PRIVMSG $chan :$nick: you could write it..."
     echo "$nick: $saying" >> ../botideas
 elif `echo $saying | grep -i 'bot \bneeds\b' > /dev/null` ; then
-    echo "PRIVMSG $chan :$nick: make a pull request :D"
+    echo "PRIVMSG $chan :$nick: make a pull request :^D"
     echo "$nick: $saying" >> ../botideas
 elif `echo $saying | grep -i 'nabb: source' > /dev/null` ; then
     echo "PRIVMSG $chan :$nick: http://goo.gl/IIQt53 is my source file"
@@ -15,14 +15,26 @@ elif `echo $saying | grep -i 'nabb: source' > /dev/null` ; then
 elif `echo $saying | grep -i '!dropthemic\b' > /dev/null` ; then
     echo "PRIVMSG $chan :OOOOH! You just got *served*!"
 elif `echo $saying | grep -i 'nabb: are you a bot?' > /dev/null` ; then
-    echo "PRIVMSG $chan :$nick: I sure am :)"
+    echo "PRIVMSG $chan :$nick: I sure am :^)"
 elif `echo $saying | grep -i '!domsmugnessquotient' > /dev/null` ; then
     echo "PRIVMSG $chan :I think we all know..."
+elif `echo $saying | grep -i 'sudo make me a sandwich' > /dev/null` ; then
+    echo "PRIVMSG $chan :That isn't the way that works and you know it :^)"
 elif `echo $saying | grep -i 'nabb: make me a sandwich' > /dev/null` ; then
     if [ "$nick" = dom ] ; then
         echo "PRIVMSG $chan :$nick: Sure thing, buddy."
     elif [ "$nick" = relsqui ] ; then
         echo "PRIVMSG $chan :$nick: Alright, I *guess* I can make you a sandwich."
+    elif [ "$nick" = sweets ] ; then
+        echo "PRIVMSG $chan :$nick: Though I cannot give you proof that sandwiches exist, I do believe they are out there, and that we are not alone in the universe."
+    elif [ "$nick" = wheezl ] ; then
+        echo "PRIVMSG $chan :$nick: OK, here you go!"
+    elif [ "$nick" = mancat ] ; then
+        echo "PRIVMSG $chan :$nick: Make your own dang sandwich, Mannius J Aberforth Catt Felinus Jr!"
+    elif [ "$nick" = tombat ] ; then
+        echo "PRIVMSG $chan :$nick: Here you go. Its a hip new combination, but its pretty underground, you've probably never heard of it."
+    elif [ "$nick" = thyme ] ; then
+        echo "PRIVMSG $chan :$nick: Alright, I guess its sandwich thyme."
     else
        echo "PRIVMSG $chan :$nick: I'll never be used by the likes of you!"
     fi        
--- nabb.bash
+++ nabb.bash
@@ -26,11 +26,6 @@ tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true ;
         barf=`echo $irc | cut -d ' ' -f 1-3`
         saying=`echo ${irc##$barf :}|tr -d "\r\n"`
         nick="${irc%%!*}"; nick="${nick#:}"
-       # cmd=`echo $saying | cut -d ' ' -f 1`
-       # args="${saying#$cmd }"
-#        if [[ $cmd == 'quit' && $nick == 'dom' ]] ; then
- #           send "QUIT :Cya!"
-  #      fi
         var=$(echo $nick $chan $saying | ./cmds.bash)
         if [[ ! -z $var ]] ; then
             send "$var"
